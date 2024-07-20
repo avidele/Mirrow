550a85f89e1af4cc62b0ed12ec1fe78af9677e8e
Avidel <1215935448@qq.com>
2024-07-20 14:15:59 +0800

    first commit

--- build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: G:/Code/workplace/mirror/test
-# Build directory: G:/Code/workplace/mirror/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("utils")
